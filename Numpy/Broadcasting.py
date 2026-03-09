7cb0a8c4a1da1c1bea2f45447940a73642a44fff
Lisa0815A <maharanalisa15@gmail.com>
2026-03-09 13:25:08 +0530

    Broadcasting error

---
+++ Numpy/Broadcasting.py
@@ -0,0 +1,12 @@
+"""
+v = [1,2,3] array = 1*3
+v1 = [1     array = 3*1 
+      2
+      3]
+1.same dimension 
+2.     
+"""
+import numpy as np
+var1 = np.array([1,2,3,4])
+var2 = np.array([1,2,3])
+print(var1 + var2)
