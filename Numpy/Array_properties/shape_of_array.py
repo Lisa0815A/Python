beb1bfbc7bfdf29068db24e8ca769beb1a553e83
Lisa0815A <maharanalisa15@gmail.com>
2026-03-11 14:24:11 +0530

    size of an array added

---
+++ Numpy/Array_properties/shape_of_array.py
@@ -0,0 +1,3 @@
+import numpy as np
+arr_shape = np.array([[1,2,3],[4,5,6]])
+print("shape of an array is :",arr_shape.shape)
---
+++ Numpy/Array_properties/size_of_array.py
@@ -0,0 +1,3 @@
+import numpy as np
+arr = np.array([[12,13,14],[9,10,11]])
+print(arr.size)
