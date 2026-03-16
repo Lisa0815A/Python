bf7e45ce4c60b872dd06f4f3fc04eb9b5b2a3b4d
Lisa0815A <maharanalisa15@gmail.com>
2026-03-16 16:15:53 +0530

    reshape of an arra

---
+++ Numpy/Array_properties/reshape.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr = np.arange(1,13)
+print("12 element array :")
+print(arr)
+#reshape into 3x4
+print("reshape above array into 3x4 :")
+matrix = arr.reshape(3,4)
+print(matrix)
