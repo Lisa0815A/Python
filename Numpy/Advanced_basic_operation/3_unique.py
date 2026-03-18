925376e6029b98f1acf5433207b4dde61339b804
Lisa0815A <maharanalisa15@gmail.com>
2026-03-18 16:01:49 +0530

    unique method added

---
+++ Numpy/Advanced_basic_operation/3_unique.py
@@ -0,0 +1,6 @@
+#Find unique elements in an array.
+import numpy as np
+a = np.array([2,3,4,2,2,2,1,3,3,4,5,4])
+print("Array :",a)
+u_el = np.unique(a)
+print("Unique element from above the array is :",u_el)
