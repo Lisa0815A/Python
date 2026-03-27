558e7111cce246dc8d257e04e9d8660d4475b116
Lisa0815A <maharanalisa15@gmail.com>
2026-03-27 08:50:01 +0530

    group by multiple column added

---
+++ Pandas/Grouping_&_aggregation/6_group_by_multiple_col.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+data = {
+    "name": ['lisa','bob','usha','bichi','pabi'],
+    "department": ['IT','HR','IT','HR','IT'],
+    "city": ['BBSR','Delhi','BBSR','Delhi','Mumbai'],
+    "salary": [50000,40000,60000,45000,70000]
+}
+
+df = pd.DataFrame(data)
+
+print(df.groupby(['department','city'])['salary'].mean())
