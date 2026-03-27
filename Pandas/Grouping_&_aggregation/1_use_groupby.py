5ad32a4ca583df79959543ae6d313c0aa15e65f1
Lisa0815A <maharanalisa15@gmail.com>
2026-03-27 08:30:06 +0530

    use groupby() added

---
+++ Pandas/Grouping_&_aggregation/1_use_groupby.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data = {
+    "name": ['lisa','bob','usha','bichi','pabi'],
+    "department": ['IT','HR','IT','HR','IT'],
+    "salary": [50000,40000,60000,45000,70000]
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+# Group by department 
+print(df.groupby('department')['salary'].agg(['mean','sum','max','min']))
