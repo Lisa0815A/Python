de96b654b73506697430115ac539833c9ba19ad1
Lisa0815A <maharanalisa15@gmail.com>
2026-03-27 08:36:41 +0530

    mean of a group added

---
+++ Pandas/Grouping_&_aggregation/2_mean.py
@@ -0,0 +1,8 @@
+import pandas as pd
+data = {
+    "name": ['lisa','bob','usha','bichi','pabi'],
+    "department": ['IT','HR','IT','HR','IT'],
+    "salary": [50000,40000,60000,45000,70000]
+}
+df = pd.DataFrame(data)
+print(df.groupby('department')['salary'].mean())
