04f212faafb9b59bbfee9e3a7763402abf0e36b3
Lisa0815A <maharanalisa15@gmail.com>
2026-03-27 16:19:29 +0530

    transform() added

---
+++ Pandas/Grouping_&_aggregation/12_transform().py
@@ -0,0 +1,10 @@
+import pandas as pd
+data = {
+  "name": ['lisa','bob','usha','bichi','pabi'],
+  "department": ['IT','HR','IT','HR','IT'],
+  "city": ['BBSR','Delhi','BBSR','Delhi','Mumbai'],
+  "salary": [50000,40000,60000,45000,70000]
+}
+df = pd.DataFrame(data)
+df['salary_doubled'] = df['salary'].transform(lambda x: x*2)
+print(df)
