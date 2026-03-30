b6fc3f29e6e830b85abfbc6a235fc62855a84e61
Lisa0815A <maharanalisa15@gmail.com>
2026-03-30 14:27:38 +0530

    pivot table added

---
+++ Pandas/Grouping_&_aggregation/19_use_pivot_table.py
@@ -0,0 +1,14 @@
+import pandas as pd
+data ={
+  "name": ["lisa", "bob", "usha", "bichi", "pabi"],
+  "department": ["IT", "HR", "IT", "HR", "IT"],
+  "city": ['BBSR','Delhi','BBSR','Delhi','Mumbai'], 
+  "salary": [50000, 40000, 60000, 45000, 70000]
+}
+df = pd.DataFrame(data)
+pivot = df.pivot_table(
+  index="department",  
+  values="salary",
+  aggfunc="mean"
+)
+print(pivot)
