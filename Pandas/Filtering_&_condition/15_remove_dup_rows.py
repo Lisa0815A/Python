f683a51bfea32ca52a2752bcfdb4e386c25cb6ce
Lisa0815A <maharanalisa15@gmail.com>
2026-04-06 19:07:23 +0530

    remove duplicate rows

--- Pandas/Filtering_&_condition/15_remove_dup_rows.py
+++ Pandas/Filtering_&_condition/15_remove_dup_rows.py
@@ -6,3 +6,17 @@ df = pd.DataFrame({
 })
 df = df.drop_duplicates()
 print(df)
+print("Last occurrence of duplicate rows:")
+df1 = pd.DataFrame({
+    'name': ['Alice', 'Bob', 'Alice', 'Charlie'],
+    'marks': [90, 60, 90, 75]
+})
+df1 = df1.drop_duplicates(keep='last') 
+print(df1) 
+print("First occurrence of duplicate rows:")
+df2 = pd.DataFrame({
+    'name': ['Alice', 'Bob', 'Alice', 'Charlie'],
+    'marks': [90, 60, 90, 75]
+})
+df2 = df2.drop_duplicates(keep='first') 
+print(df2) 
